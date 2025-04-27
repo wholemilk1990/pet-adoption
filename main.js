75306c787c413a61ce1096aeb9727c65a470cc6e
Paul <ppagliughi90@gmail.com>
2025-04-27 15:35:30 -0400

    finished pet filter buttons

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ async function petsArea() {
     const petsData = await petsPromise.json()
     petsData.forEach(pet => {
         const clone = template.content.cloneNode(true)
+        clone.querySelector(".pet-card").dataset.species = pet.species
         clone.querySelector("h3").textContent = pet.name
         clone.querySelector(".pet-description").textContent = pet.description
         clone.querySelector(".pet-age").textContent = CreateAgeText(pet.birthYear)
@@ -61,4 +62,16 @@ function buttonClick(e) {
     e.target.classList.add("selected")
 
     // filter pets in list
+    const currentFilter = e.target.dataset.filter
+    document.querySelectorAll(".pet-card").forEach(el => {
+
+        if (currentFilter == el.dataset.species || currentFilter == "all") {
+            el.style.display = "grid"
+        } else {
+            el.style.display = "none"
+        }
+    })
 }
+
+
+
